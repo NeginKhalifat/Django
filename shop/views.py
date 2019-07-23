aaf8a73015c860aa3c3846fa7d5c0a6d2ec3c547
negin khalifat <negin99kh@gmail.com>
2019-07-24 02:03:47 +0430

    improve home page

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render,redirect
 
 from django.http import HttpResponse
-
+from math import ceil
+from .models import Product
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    allProds=[]
+    catprod=Product.objects.values('category','id')
+    cats={item['category'] for item in catprod}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n//4 + ceil((n/4)-n//4)
+        allProds.append([prod,range(1,nslides),nslides])
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html',params)
 
 def about(request):
     return HttpResponse("We are at about")
